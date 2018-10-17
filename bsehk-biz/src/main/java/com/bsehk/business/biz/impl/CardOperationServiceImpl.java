ba059607ee45c735aaa735d6bd200b22e4db4095
zhang33feng <zhangfeng_xfdsn@163.com>
2018-10-17 10:57:46 +0800

    添加前36张表的service和Impl

---
+++ bsehk-biz/src/main/java/com/bsehk/business/biz/impl/CardOperationServiceImpl.java
@@ -0,0 +1,10 @@
+package com.bsehk.business.biz.impl;
+
+import com.bsehk.business.service.CardOperationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CardOperationServiceImpl implements CardOperationService {
+}
---
+++ bsehk-biz/src/main/java/com/bsehk/business/biz/impl/RoundServiceImpl.java
@@ -0,0 +1,10 @@
+package com.bsehk.business.biz.impl;
+
+import com.bsehk.business.service.RoundService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RoundServiceImpl implements RoundService {
+}

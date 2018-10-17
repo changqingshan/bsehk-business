19485b68f1cf45f2a0e77d3157f030676bbf567d
xueqinyao168 <13611163216@.com>
2018-10-17 11:24:26 +0800

    '建service'

---
+++ bsehk-biz/src/main/java/com/bsehk/business/biz/impl/VenueAdvertServiceImpl.java
@@ -0,0 +1,10 @@
+package com.bsehk.business.biz.impl;
+
+import com.bsehk.business.service.VenueAdvertService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class VenueAdvertServiceImpl implements VenueAdvertService {
+}
---
+++ bsehk-biz/src/main/java/com/bsehk/business/biz/impl/VenueBannerServiceImpl.java
@@ -0,0 +1,10 @@
+package com.bsehk.business.biz.impl;
+
+import com.bsehk.business.service.VenueBannerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class VenueBannerServiceImpl implements VenueBannerService {
+}

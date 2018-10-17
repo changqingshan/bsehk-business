bdad838e695aa696c4f3767e0d0bce9ce70450eb
zhang33feng <zhangfeng_xfdsn@163.com>
2018-10-17 16:02:39 +0800

    添加了获取体育类别二级页的接口

--- bsehk-biz/src/main/java/com/bsehk/business/biz/impl/SportCategoryServiceImpl.java
+++ bsehk-biz/src/main/java/com/bsehk/business/biz/impl/SportCategoryServiceImpl.java
@@ -19,49 +19,30 @@ public class SportCategoryServiceImpl implements SportCategoryService {
 
     @Override
     public List<SportCategoryVO> selectAllSport() {
-        SportCategoryVO sportCategoryVO = new SportCategoryVO();
-        List<SportCategoryVO> sportList = new ArrayList<SportCategoryVO>();
-        List<SportCategory> childList = new ArrayList<SportCategory>();
+        List<SportCategoryVO> sportVOList = new ArrayList<SportCategoryVO>();
 
         List<SportCategory> sportCategories = sportCategoryMapper.selectAllSport();
 
         //添加体育类的父类集合
-        /*for (SportCategory sportCategory:sportCategories) {
-            if(sportCategory.getParentId()==0){
-                sportCategoryVO.setId(sportCategory.getId());
-                sportCategoryVO.setSportName(sportCategory.getSportName());
-                sportCategoryVO.setChildList(childList);
-                sportList.add(sportCategoryVO);
-                for(SportCategoryVO sportCategoryVO1:sportList){
-                    System.out.println(sportCategoryVO1);
-                }
-            }
-        }*/
-        for(int i=0;i<sportCategories.size();i++){
-            if(sportCategories.get(i).getParentId() == 0){
+        for (int i=0;i<sportCategories.size();i++){
+            if(sportCategories.get(i).getParentId()==0){
+                SportCategoryVO sportCategoryVO = new SportCategoryVO();
                 sportCategoryVO.setId(sportCategories.get(i).getId());
                 sportCategoryVO.setSportName(sportCategories.get(i).getSportName());
+                List<SportCategory> childList = new ArrayList<SportCategory>();
                 sportCategoryVO.setChildList(childList);
-                sportList.add(sportCategoryVO);
-                System.out.println(sportCategoryVO);
+                sportVOList.add(sportCategoryVO);
             }
         }
         //为每个父类添加他的子类集合
-        /*for (SportCategoryVO parentSport:sportList) {
-            for (SportCategory sportCategory:sportCategories) {
-                if (parentSport.getId() == sportCategory.getParentId()){
-                    parentSport.getChildList().add(sportCategory);
-                }
-            }
-        }*/
-        for (int i=0;i<sportList.size();i++) {
+        for (int i=0;i<sportVOList.size();i++) {
             for (int j=0;j<sportCategories.size();j++) {
-                if (sportList.get(i).getId() == sportCategories.get(j).getParentId()){
-                    sportList.get(i).getChildList().add(sportCategories.get(j));
+                if (sportVOList.get(i).getId() == sportCategories.get(j).getParentId()){
+                    sportVOList.get(i).getChildList().add(sportCategories.get(j));
                 }
             }
         }
-        System.out.print("集合++++++++++++++++++"+sportList);
-        return sportList;
+
+        return sportVOList;
     }
 }

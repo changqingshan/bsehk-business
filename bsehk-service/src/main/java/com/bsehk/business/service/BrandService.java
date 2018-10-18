e92c95cdb4e78743ef1ba52999a86fde31dfd70c
zhang33feng <zhangfeng_xfdsn@163.com>
2018-10-18 10:22:51 +0800

    添加场馆品牌介绍接口

--- bsehk-service/src/main/java/com/bsehk/business/service/BrandService.java
+++ bsehk-service/src/main/java/com/bsehk/business/service/BrandService.java
@@ -1,7 +1,10 @@
 package com.bsehk.business.service;
 
+import com.bsehk.business.domain.Brand;
+
 /**
  * Created by 24398 on 2018/10/17.
  */
 public interface BrandService {
+    Brand selectBrandByVenueId(Long venueId);
 }
--- bsehk-web/src/main/java/com/bsehk/business/web/controller/VenueBannerController.java
+++ bsehk-web/src/main/java/com/bsehk/business/web/controller/VenueBannerController.java
@@ -2,6 +2,7 @@ package com.bsehk.business.web.controller;
 
 import com.bsehk.business.domain.VenueAdvert;
 import com.bsehk.business.domain.VenueNotice;
+import com.bsehk.business.service.BrandService;
 import com.bsehk.business.service.VenueAdvertService;
 import com.bsehk.business.service.VenueBannerService;
 import com.bsehk.business.service.VenueNoticeService;
@@ -24,25 +25,34 @@ public class VenueBannerController {
     VenueNoticeService venueNoticeService;
     @Resource
     VenueAdvertService venueAdvertService;
+    @Resource
+    BrandService brandService;
 
     //场馆banner图分类展示
     @RequestMapping("/selectBannerByVenueId")
-    public ResultData selectBannerByVenueId(Long venueId){
+    public ResultData selectBannerByVenueId(Long venueId) {
         List<VenueBannerVO> venueBannerVOList = venueBannerService.selectBannerByVenueId(venueId);
         return ResultData.success(venueBannerVOList);
     }
 
     //场馆公告展示
     @RequestMapping("/selectNoticeByVenueId")
-    public ResultData selectNoticeByVenueId(Long venueId){
+    public ResultData selectNoticeByVenueId(Long venueId) {
         VenueNotice venueNotice = venueNoticeService.selectNoticeByVenueId(venueId);
         return ResultData.success(venueNotice);
     }
 
     //场馆广告展示
     @RequestMapping("/selectAdvertByVenueId")
-    public ResultData selectAdvertByVenueId(Long venueId){
+    public ResultData selectAdvertByVenueId(Long venueId) {
         VenueAdvert venueAdvert = venueAdvertService.selectAdvertByVenueId(venueId);
         return ResultData.success(venueAdvert);
+    }
+
+    //场馆品牌介绍
+    @RequestMapping("selectBrandByVenueId")
+    public ResultData selectBrandByVenueId(Long venueId){
+
+        return ResultData.success("");
     }
 }

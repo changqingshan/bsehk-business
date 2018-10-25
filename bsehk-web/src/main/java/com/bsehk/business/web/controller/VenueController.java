9a925eefe2a13c06a015d3bd9e9a935c06566d46
xueqinyao168 <13611163216@.com>
2018-10-25 11:19:59 +0800

    整合venueController和venueHomeController

--- bsehk-web/src/main/java/com/bsehk/business/web/controller/VenueController.java
+++ bsehk-web/src/main/java/com/bsehk/business/web/controller/VenueController.java
@@ -1,12 +1,20 @@
 package com.bsehk.business.web.controller;
-import com.bsehk.business.service.VenueService;
+import com.bsehk.business.domain.Brand;
+import com.bsehk.business.domain.VenueAdvert;
+import com.bsehk.business.domain.VenueNotice;
+import com.bsehk.business.service.*;
+import com.bsehk.business.service.vo.VenueBannerVO;
+import com.bsehk.business.service.vo.VenueComplexVO;
 import com.bsehk.business.service.vo.VenueInfoVo;
 import com.bsehk.common.response.ResultData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.xml.transform.Result;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -14,6 +22,14 @@ import javax.annotation.Resource;
 public class VenueController {
     @Resource
     VenueService venueService;
+    @Resource
+    VenueBannerService venueBannerService;
+    @Resource
+    VenueNoticeService venueNoticeService;
+    @Resource
+    VenueAdvertService venueAdvertService;
+    @Resource
+    BrandService brandService;
 
     /*
      * 查询场馆信息
@@ -22,6 +38,46 @@ public class VenueController {
     public ResultData selectVenueInfoById(Long venueId) {
         VenueInfoVo venueInfoVo = venueService.selectVenueInfoById(venueId);
         return ResultData.success(venueInfoVo);
+    }
+    /**
+     * 查询场馆banner图展示
+     */
+    @RequestMapping("/selectBannerByVenueId")
+    public ResultData selectBannerByVenueId(Long venueId){
+        List<VenueBannerVO> venueBannerVOS = venueBannerService.selectBannerByVenueId(venueId);
+        return  ResultData.success(venueBannerVOS);
+    }
+    /**
+     *查询场馆公告展示
+     */
+    @RequestMapping("/selectNoticeByVenueId")
+    public ResultData selectNoticeByVenueId(Long venueId){
+        VenueNotice venueNotice = venueNoticeService.selectNoticeByVenueId(venueId, (byte) 1, false);
+        return  ResultData.success(venueNotice);
+    }
+    /**
+     * 查询场馆广告
+     */
+    @RequestMapping("/selectAdvertByVenueId")
+    public ResultData selectAdvertByVenueId(Long venueId){
+        VenueAdvert venueAdvert = venueAdvertService.selectAdvertByVenueId(venueId, false);
+        return ResultData.success(venueAdvert);
+    }
+    /**
+     * 查询场馆品牌
+     */
+    @RequestMapping("/selectBrandByVenueId")
+    public  ResultData selectBrandByVenueId(Long venueId){
+        Brand brand = brandService.selectBrandByVenueId(venueId);
+        return ResultData.success(brand);
+    }
+    /**
+     * 查询场馆详情
+     */
+    @RequestMapping("/complexInfo")
+    public ResultData complexInfo(Long venueId){
+        VenueComplexVO venueComplexInfo = venueService.getVenueComplexInfo(venueId);
+        return ResultData.success(venueComplexInfo);
     }
 }
 
